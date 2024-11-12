c2fdd5411ecc49da8d2bc0264d5177b42bf7419d
Ejiro-Frances <francesejiro@yahoo.com>
2024-11-12 18:15:36 +0100

    update

--- README.md
+++ README.md
@@ -4,11 +4,7 @@
 
 ![Design preview for the FAQ accordion coding challenge](./design/desktop-preview.jpg)
 
-## The challenge
+### Demo
 
-Users should be able to:
-
-- Hide/Show the answer to a question when the question is clicked
-- Navigate the questions and hide/show answers using keyboard navigation alone
-- View the optimal layout for the interface depending on their device's screen size
-- See hover and focus states for all interactive elements on the page
+- Solution can be seen [here](https://github.com/Ejiro-Frances/frequently-asked-questions.git)
+- Live Site can be viewed: [here](https://frequently-asked-questions-snowy.vercel.app/)
--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-const images = document.querySelectorAll(".plusImg");
-const showParagh = document.querySelectorAll(".answer-paragh");
+const faqContainers = document.querySelectorAll(".question-main-container");
 
-const updateVisibility = () => {
-  images.forEach((img) => {
-    img.addEventListener("click", () => {
-      showParagh.forEach((el) => el.classList.toggle("hide"));
-      images.classList.toggle("active");
-    });
+faqContainers.forEach((container) => {
+  // Find the specific image and paragraph within this container
+  const img = container.querySelector(".plusImg");
+  const paragraph = container.querySelector(".answer-paragh");
+
+  img.addEventListener("click", () => {
+    // Toggle visibility of the paragraph
+    paragraph.classList.toggle("hide");
+
+    // Toggle the 'active' class on the image to switch icons
+    img.classList.toggle("active");
+
+    // Change the image source based on the active class
+    if (img.classList.contains("active")) {
+      img.src = "./assets/images/icon-minus.svg"; // Set to minus image
+    } else {
+      img.src = "./assets/images/icon-plus.svg"; // Set back to plus image
+    }
   });
-  return;
-};
-updateVisibility();
+});
